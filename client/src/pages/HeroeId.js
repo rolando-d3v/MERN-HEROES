7701d8e2c51fea9cb72cdc2660fc48597547598e
rolando-R <landon3388@gmail.com>
2021-02-18 21:46:46 -0500

    dd

--- client/src/pages/HeroeId.js
+++ client/src/pages/HeroeId.js
@@ -7,9 +7,16 @@ export default function HeroeId() {
   const idx = useParams();
   const heroes = getHeroId(idx.heroId);
 
+  const {id,superhero} = heroes
+
   return (
-    <div>
-      <h3>hero id</h3>
+    <div className='flex bg-coolGray-600 ' >
+     <div>
+       <img src={`/assets/heroes/${id}.jpg`} alt=""/>
+     </div>
+     <div>
+     <h3 className='3xl font-bold text-white' >{superhero} </h3>
+     </div>
     </div>
   );
 }
