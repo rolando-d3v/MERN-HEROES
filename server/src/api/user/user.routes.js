22f065dfa06a772f9ff44e3cc707fb93731c8def
raidentek <33987212+raidentek@users.noreply.github.com>
2021-03-03 10:36:01 -0500

    red

---
+++ server/src/api/user/user.routes.js
@@ -0,0 +1,9 @@
+// const {Router} = require('express');
+import {Router} from 'express'
+
+
+
+const router = Router()
+
+
+export default router
--- server/src/app.js
+++ server/src/app.js
@@ -3,18 +3,17 @@ const morgan = require("morgan");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
+import productosRouter from "./api/productos/productos.routes";
+
 //CONFIGURACION APP
 require("./config");
-require("dotenv").config();
 
 //BASE DE DATOS
 require("./db");
 
 const app = express();
 const port = process.env.PORT;
-
-app.listen(port, () => console.log("server run in port " + port))
-
+app.listen(port, () => console.log("server run in port " + port));
 
 //MIDDLEWARES
 app.use(express.json());
@@ -23,3 +22,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan("dev"));
 app.use(cors());
+
+// ROUTES DE APP
+app.use("/productos", productosRouter);
