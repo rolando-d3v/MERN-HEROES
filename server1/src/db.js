90a2fc6b21ee0b1f1849e2948fb1c0efce4b0347
raidentek <33987212+raidentek@users.noreply.github.com>
2021-03-02 21:52:43 -0500

    red

---
+++ server1/src/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+
+
+require('./config')
+mongoose.connect(process.env.URLDB,{
+    useCreateIndex: true,
+    useUnifiedTopology:true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+},() => {
+    console.log('db conected');
+} )
