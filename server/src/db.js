31729b09580e378c93a47f018718269a92c9dfb6
raidentek <33987212+raidentek@users.noreply.github.com>
2021-03-02 21:42:42 -0500

    red

--- server/src/db.js
+++ server/src/db.js
@@ -1,16 +1,32 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const db = process.env.URLDB;
 
-mongoose.connect(
-  db,
-  {
-    useNewUrlParser: true,
+// const db = process.env.URLDB;
+
+// mongoose.connect(
+//   db,
+//   {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true,
+//   },
+//   () => console.log("db connected successfully")
+  
+// );
+
+
+
+const mongoose = require('mongoose');
+
+
+
+require('./config')
+mongoose.connect(process.env.URLDB,{
     useCreateIndex: true,
+    useUnifiedTopology:true,
+    useNewUrlParser: true,
     useFindAndModify: false,
-    useUnifiedTopology: true,
-  },
-  () => {
-    console.log("db connected successfully");
-  }
-);
+},() => {
+    console.log('db conected');
+} )
