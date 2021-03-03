4a63321fa8005e6a695f060decc69e2e9044a107
raidentek <33987212+raidentek@users.noreply.github.com>
2021-03-03 11:00:04 -0500

    red

---
+++ server/src/api/productos/productoController.js
@@ -0,0 +1,15 @@
+export const createProductos = async (req, res) => {
+res.json('hola mundo')
+}
+export const getProductos = async (req, res) => {
+res.json('hola mundo')
+}
+export const getProductoId = async (req, res) => {
+res.json('hola mundo')
+}
+export const updateProductos = async (req, res) => {
+res.json('hola mundo')
+}
+export const deleteProductos = async (req, res) => {
+res.json('hola mundo')
+}
--- server/src/api/productos/productos.routes.js
+++ server/src/api/productos/productos.routes.js
@@ -1,8 +1,13 @@
 import {Router} from 'express'
+import * as productoCtrl from './productoController'
 
 const router = Router()
 
-router.get('/', (req, res)=>  res.json({message: 'hola'}) )
+router.post('/', productoCtrl.createProductos)
+router.get('/', productoCtrl.getProductos )
+router.get('/:productoId', productoCtrl.getProductoId )
+router.put('/:productoId', productoCtrl.updateProductos)
+router.delete('/productoId', productoCtrl.deleteProductos )
 
 
 export default router
