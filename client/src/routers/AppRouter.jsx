13c59e9bebc758f260b9a08655b5073dc352d511
unknown <landon3388@gmail.com>
2021-12-31 17:06:01 -0500

    red

---
+++ client/src/routers/AppRouter.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+
+export default function AppRouter() {
+    return (
+        <div>
+            <Routes>
+
+                <Route path='/'  element={} />
+
+
+            </Routes>
+            
+        </div>
+    )
+}
